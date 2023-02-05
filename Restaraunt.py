e383a5f28be4b31ba85f9398c3191fe1ab8051b0
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-02-05 15:18:52 -0500

    Create Restaraunt.py

---
+++ Restaraunt.py
@@ -0,0 +1,11 @@
+print("Thank you for coming in today!!. This is Jackers Restaurant")
+
+name  = input ("what is your name? ")
+
+print ("Hello " + name + ", its great having you here what would you like to order\n" )
+
+print ("- We have a variable of options\n" + "steak\n" + "fries\n" )
+
+order = input ()
+
+print ("Great " + name + ", we would get " + order + " ready for you in a second")
