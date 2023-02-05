cf3722f1118b56daefe57a2d8528d9d0843b0b9f
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-02-05 16:03:25 -0500

    Update Restaraunt.py

--- Restaraunt.py
+++ Restaraunt.py
@@ -4,8 +4,16 @@ name  = input ("what is your name? ")
 
 print ("Hello " + name + ", its great having you here what would you like to order\n" )
 
-print ("- We have a variable of options\n" + "steak\n" + "fries\n" )
+print ("- We have a variable of options\n" + "steaks\n" + "fries\n" )
 
 order = input ()
 
-print ("Great " + name + ", we would get " + order + " ready for you in a second")
+price = 8
+
+quantity = input("I see you got friends, for how many people? ")
+
+total = price * int(quantity)
+
+print("Thank you your total is : $" + str(total))
+
+print ("Great " + name + ", we would get " + quantity + " " + order + " ready for you in a second, pls get seated")
