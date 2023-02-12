38709a675d6d23fb1b98111cdb63d513f2b2de06
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-02-12 06:54:24 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -9,7 +9,7 @@ supplies = ["spoon", "fork", "frying pan", "pot", "oven", "dishwasher"]
 restraunt_list = ["spoon" , "fork" , 30 , 22.9 , True]
 
 #me = restraunt_utensils[0]
-#when you use a - in a list it basically starts from the back and counts.. so the last thing in the list would be given the value -1 second to last would be -2
+#when you use a -in a list it basically starts from the back and counts.. so the last thing in the list would be given the value -1 second to last would be -2
 #you = restraunt_utensils[-2]
 #print (me)
 #print (you)
@@ -21,6 +21,13 @@ restraunt_list = ["spoon" , "fork" , 30 , 22.9 , True]
 #extend allows you to add more than one item to a list it needs  to be ([data])
 #supplies.extend(["toilet paper" , "bidet"])
 
-supplies = supplies + ["toilet paper", "bidet"]
+supplies.insert(0,"bidet")
+supplies.insert(-1,"toilet paper")
 
+#the clear method is used to delete a list completely
+#supplies.clear()
+#the remove method is used to remove a specific object in a list 
+#supplies.remove ("spoon")
+print("this item was just deleted " + supplies.pop(0))
+#print(supplies.pop(0))
 print (supplies)
