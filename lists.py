6a3bda666eabacd9f393e81fd0e13e2e42d2f33f
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-02-12 05:39:04 -0500

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,15 @@
+#making a list in python
+#restraunt_utensils = "spoon, fork, frying pan, pot, oven, dishwasher"
+
+#print (restraunt_utensils)
+
+restraunt_utensils = ["spoon", "fork", "frying pan", "pot", "oven", "dishwasher"]
+
+#i just assigned values to the list 
+restraunt_list = ["spoon" , "fork" , 30 , 22.9 , True]
+
+#its called indexing or ordering in a list
+me = restraunt_utensils[0]
+you = restraunt_utensils[3]
+print (me)
+print (you)
