54993fc7f4f0c320a2b71749e532ae4f9dcfd35e
agentjacker <114850373+agentjacker@users.noreply.github.com>
2023-02-12 05:10:50 -0500

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,9 @@
+#booleans
+print ("hello, welcome to agentjacker restraunt!!")
+
+name = input ("what is your name?")
+
+if not name == "ben":
+  print ("get out of here " + name)
+else:
+  print ("come on in")
